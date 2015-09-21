902be5d82697d7102436ba250776952d35af95f7
power1588 <power15@126.com>
2015-09-21 12:23:35 +1200

    using numerical method to get the gradient

--- NumericalDiffer1.R
+++ NumericalDiffer1.R
@@ -3,3 +3,12 @@ h <- 0.001
 x0 <- 1
 err <- 1- ((x0 + h)^(x0 + h ) - x0^x0)/h
 err
+
+err1 <- 1- ((x0 + h)^(x0 + h ) - (x0 - h)^(x0 - h )) /(2*h)
+err1
+
+require(numDeriv)
+f <- function(x ) x^x
+grad(f,x=1)
+
+grad(f,x = 1, method ="simple")
